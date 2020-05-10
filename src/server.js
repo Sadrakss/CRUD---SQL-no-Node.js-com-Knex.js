e5c1852ffa2289079c5cabba3c28fadd3f011b77
sadrakss <sadrakss@outlook.com.br>
2020-05-10 16:35:00 -0300

    user crud

--- src/server.js
+++ src/server.js
@@ -6,6 +6,21 @@ const routes = require('./routes')
 app.use(express.json())
 app.use(routes)
 
+
+//not Found
+app.use((req,res,next) =>{
+    const error = new Error('not found')
+    error.status = 404
+    next(error)
+})
+
+// catch all
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({error:error.message})
+})
+
+
 app.listen(3333, () => {
     console.log(`Server running on PORT 3333`)
 })
